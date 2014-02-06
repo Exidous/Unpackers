3845f028e04754f8fd14dc934c458ba34efe4a2f
Exidous <Exidous2008@gmail.com>
2014-02-05 22:02:36 -0600

    Fixed label

--- Unpackers/Unpackers/Form1.Designer.vb
+++ Unpackers/Unpackers/Form1.Designer.vb
@@ -39,11 +39,12 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(28, 9)
+        Me.Label1.Location = New System.Drawing.Point(2, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(361, 13)
+        Me.Label1.Size = New System.Drawing.Size(417, 13)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Supports: AsPack, PeCompact, UPX, Mpress, FSG, Petite, NeoLite && E-Zip"
+        Me.Label1.Text = "Supports: AsPack, PeCompact, UPX, Mpress, FSG, Petite, NeoLite, E-Zip && SecuPack" & _
+    ""
         '
         'OpenFileDialog1
         '
@@ -53,7 +54,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(406, 68)
+        Me.ClientSize = New System.Drawing.Size(420, 68)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
